5aa47999093644320f0d49d22c2b4d9802967ef9
syedamir5560 <syedamir5560@gmail.com>
2024-04-19 15:42:04 +0530

    s

--- src/Reducer/filterReducer.js
+++ src/Reducer/filterReducer.js
@@ -69,6 +69,24 @@ const filterReducer = (state, action) => {
                     [name]: value,
                   },
                 };
+
+             
+                    case "FILTER_PRODUCTS":
+                        let { all_products } = state;
+                        let tempFilterProduct = [...all_products];
+                  
+                        const { text, category, company, color } = state.filters;
+                  
+                        if (text) {
+                          tempFilterProduct = tempFilterProduct.filter((curElem) => {
+                            return curElem.name.toLowerCase().includes(text);
+                          });
+                        }
+
+                        return {
+                            ...state,
+                            filter_products: tempFilterProduct,
+                          };
             
         default:
             return state
